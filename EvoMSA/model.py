1391b22f62540732df826a2b3738fdec90f965c8
Mario Graff <mgraffg@ieee.org>
2018-03-13 10:30:59 -0600

    copyright

--- EvoMSA/model.py
+++ EvoMSA/model.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Mario Graff Guerrero
+# Copyright 2018 Mario Graff Guerrero
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
