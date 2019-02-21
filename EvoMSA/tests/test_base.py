7119f441ad918bc747db746e3e23008dca1b8245
Mario Graff <mgraffg@ieee.org>
2019-02-21 14:55:59 -0600

    tests

--- EvoMSA/tests/test_base.py
+++ EvoMSA/tests/test_base.py
@@ -167,28 +167,6 @@ def test_binary_labels_json():
     print(_)
 
 
-def test_EvoMSA_predict_proba_logistic_regression():
-    X, y = get_data()
-    evo = EvoMSA(logistic_regression=True,
-                 evodag_args=dict(popsize=100, early_stopping_rounds=100, time_limit=5,
-                                  n_estimators=5),
-                 n_jobs=2).fit([X, [x for x, y0 in zip(X, y) if y0 in ['P', 'N']]],
-                               [y, [x for x in y if x in ['P', 'N']]])
-    hy = evo.predict_proba(X)
-    assert len(hy) == 1000
-    assert hy.min() >= 0 and hy.max() <= 1
-    print(hy)
-
-
-def test_EvoMSA_logistic_regression_params():
-    X, y = get_data()
-    evo = EvoMSA(logistic_regression=True, logistic_regression_args=dict(C=10),
-                 evodag_args=dict(popsize=100, early_stopping_rounds=100, time_limit=5,
-                                  n_estimators=5),
-                 n_jobs=2)
-    assert evo._logistic_regression.C == 10
-
-
 def test_EvoMSA_exogenous_model():
     X, y = get_data()
     model = EvoMSA(evodag_args=dict(popsize=10, early_stopping_rounds=10),
