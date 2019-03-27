606e4f0dd26caee6012cf66f88aa1fb237e6b73e
Mario Graff <mgraffg@ieee.org>
2019-03-27 13:47:16 -0600

    Spaces

--- EvoMSA/model.py
+++ EvoMSA/model.py
@@ -246,7 +246,7 @@ class LabeledDataSet(BaseTextModel, BaseClassifier):
         self._coef = coef
         self._intercept = intercept
         self._labels = labels
-        self._text = os.getenv('TEXT', default='text')        
+        self._text = os.getenv('TEXT', default='text')
 
     @property
     def textModel(self):
