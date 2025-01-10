147866ba2a88ad7ae7964a5f68fb9bc31f59e45f
Mario Graff <mgraffg@ieee.org>
2025-01-10 08:42:58 -0600

    Missing import in init

--- EvoMSA/__init__.py
+++ EvoMSA/__init__.py
@@ -14,6 +14,6 @@
 __version__ = '2.0.11'
 
 try:
-    from EvoMSA.text_repr import BoW, TextRepresentations, StackGeneralization, DenseBoW
+    from EvoMSA.text_repr import BoW, TextRepresentations, StackGeneralization, DenseBoW, BoWT, DenseBoWT
 except ModuleNotFoundError:
     pass
